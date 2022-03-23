b394960378e210b18fecba1ce3d9a7f4954553cd
Pam <rpamdusu@yahoo.com>
2022-03-23 22:43:20 +0000

    FIrst Push

---
+++ app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('cFings')
+const menuLinks = document.querySelector('finger')
+const menuLinks =
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+
+    })
