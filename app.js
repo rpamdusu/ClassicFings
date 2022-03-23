4aaee4e0ddaa9760dbdd0c1f51de27074daf0332
Pam <rpamdusu@yahoo.com>
2022-03-23 23:03:56 +0000

    New Push

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
+
+function classFings() {
+    // Initialize an agent at application startup.
+    const fpPromise = FingerprintJS.load({
+        apiKey: 'Rj1uQxYPXTNOS7czdJyD',
+        region: 'eu'
+    })
+
+    // Get the visitor identifier when you need it.
+    fpPromise
+        .then(fp => fp.get())
+        .then(result => console.log(result.visitorId))
+}
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
-const menu = document.querySelector('cFings')
-const menuLinks = document.querySelector('finger')
-const menuLinks =
-    menu.addEventListener('click', function() {
-        menu.classList.toggle('is-active');
-        menuLinks.classList.toggle('active');
 
-    })
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+
+})
