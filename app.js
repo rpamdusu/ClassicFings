58147621a93121270e3246ea31c4900fe78a6771
Pam <rpamdusu@yahoo.com>
2022-03-24 13:44:22 +0000

    CLientJS Push

--- app.js
+++ app.js
@@ -18,6 +18,83 @@ function classFings() {
         .then(result => console.log(result.visitorId))
         //Link the page to deliver the results 
     document.getElementById("finger").onclick = function() {
+        client.getBrowserData();
+        client.getFingerprint();
+        client.getCustomFingerprint();
+
+        client.getUserAgent();
+        client.getUserAgentLowerCase();
+
+        client.getBrowser();
+        client.getBrowserVersion();
+        client.getBrowserMajorVersion();
+        client.isIE();
+        client.isChrome();
+        client.isFirefox();
+        client.isSafari();
+        client.isOpera();
+
+        client.getEngine();
+        client.getEngineVersion();
+
+        client.getOS();
+        client.getOSVersion();
+        client.isWindows();
+        client.isMac();
+        client.isLinux();
+        client.isUbuntu();
+        client.isSolaris();
+
+        client.getDevice();
+        client.getDeviceType();
+        client.getDeviceVendor();
+
+        client.getCPU();
+
+        client.isMobile();
+        client.isMobileMajor();
+        client.isMobileAndroid();
+        client.isMobileOpera();
+        client.isMobileWindows();
+        client.isMobileBlackBerry();
+
+        client.isMobileIOS();
+        client.isIphone();
+        client.isIpad();
+        client.isIpod();
+
+        client.getScreenPrint();
+        client.getColorDepth();
+        client.getCurrentResolution();
+        client.getAvailableResolution();
+        client.getDeviceXDPI();
+        client.getDeviceYDPI();
+
+        client.getPlugins();
+        client.isJava();
+        client.getJavaVersion(); // functional only in java and full builds, throws an error otherwise
+        client.isFlash();
+        client.getFlashVersion(); // functional only in flash and full builds, throws an error otherwise
+        client.isSilverlight();
+        client.getSilverlightVersion();
+
+        client.getMimeTypes();
+        client.isMimeTypes();
+
+        client.isFont();
+        client.getFonts();
+
+        client.isLocalStorage();
+        client.isSessionStorage();
+        client.isCookie();
+
+        client.getTimeZone();
+
+        client.getLanguage();
+        client.getSystemLanguage();
+
+        client.isCanvas();
+        client.getCanvasPrint();
         location.href = "tech.html";
     };
 }
